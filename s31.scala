a3577f3e1b0dbec9c207702382ba95c612f55d06
Creed Allen <allenc05@gmail.com>
2016-03-31 09:54:08 -0400

    Added files via upload

--- s31.scala
+++ s31.scala
@@ -1,6 +1,12 @@
-val currency = List(1, 2, 5, 10, 20, 50, 100, 200)
-
-// Find the max possible ways to generate 200
-
-// Move down starting with the 2 lb denomination
-
+// DP Solution
+def dp_currency_combos(total: Int, denominations: List[Int]): Int = {
+	val table = Array.ofDim[Int](total + 1)
+	table(0) = 1 // 1 way to make a total of 0
+	for (i <- 0 until denominations.length;
+		j <- denominations(i) to total) {
+		table(j) += table(j - denominations(i))
+	} 
+	table(total)
+}
+val denominations = List(1, 2, 5, 10, 20, 50, 100, 200)
+println(dp_currency_combos(200, denominations))
